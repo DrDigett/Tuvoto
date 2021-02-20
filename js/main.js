ccbb83d9d9f0e2322a416aa2b6a75f0bac4c4116
Diego <d9730707@gmail.com>
2021-02-19 20:09:24 -0500

    termine con el burger menu

--- js/main.js
+++ js/main.js
@@ -21,13 +21,7 @@ $(document).ready(main);
      $('.submenu').click(function(){
          $(this).children('.children').slideToggle();
      });
-     $('nav ul').click(function(){
-      if(contador == 0){
-          $('nav').animate({
-              left: '-100%'
-          })
-      }
-  })
+     
  }
 
 
