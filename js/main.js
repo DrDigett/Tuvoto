61c424cf6939013139430cf41233181590abd620
Diego <d9730707@gmail.com>
2021-02-19 20:02:05 -0500

    termine con el burger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+$(document).ready(main);
+ 
+ var contador = 1;
+  
+ function main () {
+     $('.menu_bar').click(function(){
+         if (contador == 1) {
+             $('nav').animate({
+                 left: '0'
+             });
+             contador = 0;
+         } else {
+             contador = 1;
+             $('nav').animate({
+                 left: '-100%'
+             });
+         }
+     });
+  
+     // Mostramos y ocultamos submenus
+     $('.submenu').click(function(){
+         $(this).children('.children').slideToggle();
+     });
+ }
+
+
+
+
+
 var slides = document.querySelectorAll('.slide');
 var btns = document.querySelectorAll('.btn');
 let currentSlide = 1;
@@ -50,29 +79,3 @@ var repeat = function(activeClass){
   repeater();
 }
 repeat();
-
-$(document).ready(main);
- 
- var contador = 1;
-  
- function main () {
-     $('.menu_bar').click(function(){
-         if (contador == 1) {
-             $('nav').animate({
-                 left: '0'
-             });
-             contador = 0;
-         } else {
-             contador = 1;
-             $('nav').animate({
-                 left: '-100%'
-             });
-         }
-     });
-  
-     // Mostramos y ocultamos submenus
-     $('.submenu').click(function(){
-         $(this).children('.children').slideToggle();
-     });
- }
-
